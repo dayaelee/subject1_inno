16e8b172dcbf15817dbb0626a9f3fef9fd912d95
dayaelee <dayae.dev@gmail.com>
2025-07-11 15:10:43 +0900

    [Level1-5]연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class App {
-
+    public static int[] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        arr = new int[10];
+        int cnt = 0;
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -20,21 +21,30 @@ public class App {
 
             if (cal == ('+')) {
                 result = num1 + num2;
+                arr[cnt] = result;
                 System.out.println("결과: " + result);
+                cnt+=1;
             } else if (cal == ('-')) {
                 result = num1 - num2;
+                arr[cnt] = result;
                 System.out.println("결과: " + result);
+                cnt+=1;
             } else if (cal == ('*')) {
                 result = num1 * num2;
+                arr[cnt] = result;
                 System.out.println("결과: " + result);
+                cnt+=1;
             } else if (cal == ('/')) {
                 try {
                     result = num1 / num2;
+                    arr[cnt] = result;
                     System.out.println("결과: " + result);
+                    cnt+=1;
                 } catch (ArithmeticException e) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
                 }
             }
+
 
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
