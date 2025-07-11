d0508b1851bf38dcf4831476be6dfbf94c6ced3c
dayaelee <dayae.dev@gmail.com>
2025-07-11 15:36:46 +0900

    [Level1-7]연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    public static int[] arr;
+    public static List<Integer> arr;
     public static int result;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        arr = new int[10];
+        arr = new ArrayList<>();
+
         int cnt = 0;
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -23,54 +27,58 @@ public class App {
 
             if (cal == ('+')) {
                 result = num1 + num2;
-                if (cnt >= 10){
-                    for (int i = 1; i <= 9; i++) {
-                        arr[i-1] = arr[i];
-                    }
-                    arr[9]=result;
-                }else {
-                    arr[cnt] = result;
-                    cnt+=1;
-                }
+//                if (cnt >= 10){
+//                    for (int i = 1; i <= 9; i++) {
+//                        arr.[i-1] = arr[i];
+//                    }
+//                    arr[9]=result;
+//                }else {
+//                    arr[cnt] = result;
+//                    cnt+=1;
+//                }
+                arr.add(result);
                 System.out.println("결과: " + result);
 
             } else if (cal == ('-')) {
                 result = num1 - num2;
-                if (cnt >= 10){
-                    for (int i = 1; i <= 9; i++) {
-                        arr[i-1] = arr[i];
-                    }
-                    arr[9]=result;
-                }else {
-                    arr[cnt] = result;
-                    cnt+=1;
-                }
+//                if (cnt >= 10){
+//                    for (int i = 1; i <= 9; i++) {
+//                        arr[i-1] = arr[i];
+//                    }
+//                    arr[9]=result;
+//                }else {
+//                    arr[cnt] = result;
+//                    cnt+=1;
+//                }
+                arr.add(result);
                 System.out.println("결과: " + result);
 
             } else if (cal == ('*')) {
                 result = num1 * num2;
-                if (cnt >= 10){
-                    for (int i = 1; i <= 9; i++) {
-                        arr[i-1] = arr[i];
-                    }
-                    arr[9]=result;
-                }else {
-                    arr[cnt] = result;
-                    cnt += 1;
-                }
+//                if (cnt >= 10){
+//                    for (int i = 1; i <= 9; i++) {
+//                        arr[i-1] = arr[i];
+//                    }
+//                    arr[9]=result;
+//                }else {
+//                    arr[cnt] = result;
+//                    cnt += 1;
+//                }
+                arr.add(result);
                 System.out.println("결과: " + result);
             } else if (cal == ('/')) {
                 try {
                     result = num1 / num2;
-                    if (cnt >= 10){
-                        for (int i = 1; i <= 9; i++) {
-                            arr[i-1] = arr[i];
-                        }
-                        arr[9]=result;
-                    }else {
-                        arr[cnt] = result;
-                        cnt += 1;
-                    }
+//                    if (cnt >= 10){
+//                        for (int i = 1; i <= 9; i++) {
+//                            arr[i-1] = arr[i];
+//                        }
+//                        arr[9]=result;
+//                    }else {
+//                        arr[cnt] = result;
+//                        cnt += 1;
+//                    }
+                    arr.add(result);
                     System.out.println("결과: " + result);
 
                 } catch (ArithmeticException e) {
@@ -79,13 +87,20 @@ public class App {
             }
 
 
-            System.out.println(Arrays.toString(arr));
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String check0 = sc.nextLine();
+            if (check0.equals("remove")){
+                arr.remove(0);
+            }
+
+            System.out.println(arr.toString());
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String check = sc.nextLine();
             if (check.equals("exit")){
                 break;
             }
+
         }
 
 
